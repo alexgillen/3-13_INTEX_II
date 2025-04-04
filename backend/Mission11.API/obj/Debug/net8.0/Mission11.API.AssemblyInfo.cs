489d35dceb5414d0f94cbd34d28bc1432d7b9592
Dave Gillen <162204811+alexgillen@users.noreply.github.com>
2025-04-03 18:47:47 -0600

    Added routes.json

--- backend/Mission11.API/obj/Debug/net8.0/Mission11.API.AssemblyInfo.cs
+++ backend/Mission11.API/obj/Debug/net8.0/Mission11.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c004f5ead17986a71b0d5dc5248ff7aea7e9416b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1d9a8a45ef9711e02c2cd8ed7dbb0161d35e427")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
