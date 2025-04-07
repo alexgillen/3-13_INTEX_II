6f50e23a2b0b2300c7dbeda5d94f6ab56f03da0a
Dave Gillen <162204811+alexgillen@users.noreply.github.com>
2025-04-07 10:27:53 -0600

    Init03

--- backend/INTEX_II.API/obj/Debug/net8.0/Mission11.API.AssemblyInfo.cs
+++ backend/INTEX_II.API/obj/Debug/net8.0/Mission11.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33bf69bbf8dcc640d55a91fb9e2cc38da596301c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccfb48b9b15763add00d5415a4fe35f2d64f9b3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
